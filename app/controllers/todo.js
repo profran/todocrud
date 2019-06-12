9e4d4dd8b688bec4c1689a4020d99cb5eed3bd3f
Francesco Silvetti <francescosilvetti8@gmail.com>
2019-06-11 22:19:54 -0300

    Fix deleteById

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -11,7 +11,7 @@ module.exports = {
     })
   },
   deleteById: function (req, res, next) {
-    todoModel.deleteById(req.body.todoId, function (err, todo) {
+    todoModel.findByIdAndDelete(req.body.todoId, function (err, todo) {
       if (err) {
         next(err)
       } else {
